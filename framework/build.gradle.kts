ecbd5d031d89059e5731f38c46e8358556fc751d
Raghav Awasthi <raghavawasthi2014@gmail.com>
2023-01-31 15:10:27 +0530

    Fixed Jetpack builds

--- framework/build.gradle.kts
+++ framework/build.gradle.kts
@@ -1,6 +1,4 @@
 import dependencies.Dependencies
-
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -46,7 +44,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
     implementation(project(mapOf("path" to ":data")))
-    implementation ("com.raghav.common:common-utils:0.0.1-SNAPSHOT")
+    implementation(project(mapOf("path" to ":common")))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
